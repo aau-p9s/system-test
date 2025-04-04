46eb049d1c0bfafe1f0d4d7948514f078535890b
Thomas Jensen <mast3r@skade.dev>
2025-04-04 10:22:17 +0200

    fix testcase

--- tests/Baseline.py
+++ tests/Baseline.py
@@ -1,7 +1,7 @@
 from lib.TestCase import TestCase
 
 class Baseline(TestCase):
-    def __init__(self, size:dict[str, int] = {"x":4000, "y":4000}, period:int = 86400, delay:int = 10, tests:int = 3):
+    def __init__(self, size:dict[str, int] = {"x":4000, "y":4000}, period:int = 86400, delay:int = 25, tests:int = 3):
         super().__init__(size, period, delay, tests)
 
     def setup_HPA(self):
