bced544b86d3dd3c2e0b2165091a6b07ddbfb999
Thomas Jensen <mast3r@skade.dev>
2025-04-09 14:23:46 +0200

    fixed data

---
+++ lib/data.py
@@ -0,0 +1,75 @@
+workload_api_deployment = lambda memory_req, cpu_req, memory_lim, cpu_lim: {
+    "apiVersion": "apps/v1",
+    "kind": "Deployment",
+    "metadata": {
+        "name": "workload-api-deployment"
+    },
+    "spec": {
+        "selector": {
+            "matchLabels": {
+                "app": "workload-api"
+            }
+        },
+        "template": {
+            "metadata": {
+                "labels": {
+                    "app": "workload-api"
+                }
+            },
+            "spec": {
+                "containers": [
+                    {
+                        "name": "workload-api",
+                        "image": "ghcr.io/aau-p9s/workload-api:latest",
+                        "environment": {
+                            "WORKLOAD_PORT": 8123
+                        },
+                        "ports": [
+                            {
+                                "containerPort": 8123
+                            }
+                        ],
+                        "resources": {
+                            "requests": {
+                                "memory": memory_req,
+                                "cpu": cpu_req
+                            },
+                            "limits": {
+                                "memory": memory_lim,
+                                "cpu": cpu_lim
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
+}
+hpa_patch = lambda max, min, scale_up, scale_down: {
+    "spec":{
+        "maxReplicas":max,
+        "minReplicas":min,
+        "behavior": {
+            "scaleUp": {
+                "policies": [
+                    {
+                        "type": "Percent",
+                        "value": int(scale_up*100),
+                        "periodSeconds": 60
+                    }
+                ],
+                "stabilizationWindowSeconds": 300
+            },
+            "scaleDown": {
+                "policies": [
+                    {
+                        "type": "Percent",
+                        "value": int(scale_down*100),
+                        "periodSeconds": 60
+                    }
+                ],
+                "stabilizationWindowSeconds": 300
+            }
+        }
+    },
+}
--- tests/GroundTruth.py
+++ tests/GroundTruth.py
@@ -2,38 +2,21 @@ from json import dumps
 import os
 from time import sleep
 from lib.TestCase import TestCase
+from lib.data import hpa_patch
 
 class GroundTruth(TestCase):
     def kubernetes_setup(self):
-        os.system("kubectl autoscale deployment workload-api-deployment --cpu-percent=50 --min=1 --max=10")
-        print("deployed autoscaler")
-        data = {
-            "spec":{
-                "maxReplicas":self.max_replicas,
-                "minReplicas":self.min_replicas,
-                "behavior": {
-                    "scaleUp": {
-                        "policies": [
-                            {
-                                "type": "Percent",
-                                "value": int(self.scale_up*100),
-                                "periodSeconds": 60
-                            }
-                        ],
-                        "stabilizationWindowSeconds": 300
-                    },
-                    "scaleDown": {
-                        "policies": [
-                            {
-                                "type": "Percent",
-                                "value": int(self.scale_up*100),
-                                "periodSeconds": 60
-                            }
-                        ],
-                        "stabilizationWindowSeconds": 300
-                    }
-                }
-            },
-        }
-        os.system(f"kubectl patch hpa workload-api-deployment --patch '{dumps(data)}'")
+        #print("Cleaning kubernetes autoscalers and deployments")
+        #os.system("""
+        #    kubectl delete hpa workload-api-deployment
+        #    kubectl delete deployment workload-api-deployment
+        #""")
+        #print("Deploying workload")
+        #os.system(f"echo '{dumps(workload_api_data)}' | kubectl apply -f -")
+        #print("deployed workflow api")
+        #os.system("kubectl autoscale deployment workload-api-deployment --cpu-percent=50 --min=1 --max=10")
+        #print("deployed autoscaler")
+        ## Patch HPA
+        patch_data = hpa_patch(self.max_replicas, self.min_replicas, self.scale_up, self.scale_down)
+        os.system(f"kubectl patch hpa workload-api-deployment --patch '{dumps(patch_data)}'")
         sleep(120)
