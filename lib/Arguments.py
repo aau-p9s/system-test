8ab184accd191dd547229b27da82a8d96ea1ca84
Thomas Jensen <mast3r@skade.dev>
2025-05-20 22:20:23 +0200

    fix verbose flag

--- lib/Arguments.py
+++ lib/Arguments.py
@@ -4,7 +4,7 @@ import sys
 parser = argparse.ArgumentParser(sys.argv[0])
 parser.add_argument("target")
 parser.add_argument("target_deployment")
-parser.add_argument("--verbose", "-v", type=bool, default=False)
+parser.add_argument("--verbose", "-v", action="store_true")
 
 args = vars(parser.parse_args())
 target = args["target"]
