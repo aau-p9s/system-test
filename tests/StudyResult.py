2741cfcc8ec32ecd29c721681521bbd1ee1ac29d
Thomas Jensen <mast3r@skade.dev>
2025-05-20 19:35:18 +0200

    fix deployment

--- tests/StudyResult.py
+++ tests/StudyResult.py
@@ -12,23 +12,32 @@ postgres_port = 5432
 class StudyResult(TestCase):
     def kubernetes_setup(self):
         autoscaler_kubeconfig = autoscaler_deployment("autoscaler", "root", "password", postgres_port, autoscaler_port, forecaster_port)
+        late_deployments = []
         for kubeconfig in autoscaler_kubeconfig:
+            if kubeconfig["metadata"]["name"] in ["autoscaler", "forecaster"] and kubeconfig["kind"] == "Deployment":
+                late_deployments.append(kubeconfig)
+            else:
+                system(f"echo '{dumps(kubeconfig)}' | kubectl apply -f -")
+
+        system("""
+            rm -rf /tmp/autoscaler /tmp/forecaster
+            git clone https://github.com/aau-p9s/autoscaler /tmp/autoscaler
+            git clone -b "feat/model_deployment_scripts" https://github.com/aau-p9s/forecaster /tmp/forecaster
+            cp -r /tmp/autoscaler/Autoscaler.Api/BaselineModels /tmp/forecaster/Assets/models
+            nix run path:/tmp/forecaster#reinit
+            nix run path:/tmp/forecaster#deploy
+        """)
+
+        # Do the late deployments
+        for kubeconfig in late_deployments:
             system(f"echo '{dumps(kubeconfig)}' | kubectl apply -f -")
         # Wait for deployments ready
         system("""
             kubectl wait --for=condition=Available deployments/autoscaler
             kubectl wait --for=condition=Available deployments/forecaster
         """)
-        system("""
-            rm -rf /tmp/autoscaler /tmp/forecaster
-            git clone https://github.com/aau-p9s/autoscaler /tmp/autoscaler
-            git clone -b "feat/model_deployment_scripts" https://github.com/aau-p9s/forecaster /tmp/forecaster
-            mkdir -p /tmp/forecaster/Assets/models
-            cp -r /tmp/autoscaler/Autoscaler.Api/BaselineModels/* /tmp/forecaster/Assets/models/
-            nix run path:/tmp/forecaster#reinit
-            nix run path:/tmp/forecaster#deploy
-        """)
-        system("curl localhost:{autoscaler_port}/services/start")
+
+        system(f"curl localhost:{autoscaler_port}/services/start")
         services = loads(check_output(["curl", f"localhost:{autoscaler_port}/services"]).decode())
         service_id = ""
         for i, service in enumerate(services):
