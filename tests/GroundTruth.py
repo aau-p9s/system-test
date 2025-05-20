e639351dc98e9cfd07292efa586ce8d387790cee
Thomas Jensen <mast3r@skade.dev>
2025-05-20 18:57:31 +0200

    removed deployments

--- tests/GroundTruth.py
+++ tests/GroundTruth.py
@@ -6,6 +6,9 @@ from lib.data import hpa_patch
 
 class GroundTruth(TestCase):
     def kubernetes_setup(self):
+        os.system("""
+            kubectl delete deployment autoscaler forecaster
+        """)
         os.system(f"kubectl autoscale deployment {self.target_deployment} --cpu-percent=50 --min=1 --max=10")
         print("deployed autoscaler")
         # Patch HPA
