030dddc269eb09d43aa16c857d6b4670728e4039
Thomas Jensen <mast3r@skade.dev>
2025-05-20 15:47:54 +0200

    fixed dump

--- tests/StudyResult.py
+++ tests/StudyResult.py
@@ -1,6 +1,7 @@
 from os import system
 from time import sleep
 from lib.TestCase import TestCase
+from json import dumps
 from lib.data import autoscaler_deployment
 
 wait_time = 10
@@ -9,7 +10,7 @@ class StudyResult(TestCase):
     def kubernetes_setup(self):
         autoscaler_kubeconfig = autoscaler_deployment("autoscaler", "root", "password", "5432", "8080", "8081")
         for kubeconfig in autoscaler_kubeconfig:
-            system(f"echo '{kubeconfig}' | kubectl apply -f -")
+            system(f"echo '{dumps(kubeconfig)}' | kubectl apply -f -")
         print("Waiting for {wait_time} seconds")
         sleep(wait_time)
         system("rm -rf /tmp/autoscaler /tmp/forecaster")
