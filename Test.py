f141306bb489becb8d1487e9c861ae533519a9bd
Thomas Jensen <mast3r@skade.dev>
2025-05-22 01:07:04 +0200

    streamline similar tests

--- Test.py
+++ Test.py
@@ -1,41 +1,35 @@
 from os import get_terminal_size
 from lib.TestCase import TestCase
-from tests.Baseline import Baseline
-from tests.GroundTruth import GroundTruth
-from tests.StudyResult import StudyResult
+from tests.Utils import make_test_triple
 import lib.Arguments as args # force entire module to load
 
-print(f"argparse name: {args.parser.prog}")
+def headline(title: str) -> str:
+    total = get_terminal_size()[0]
+    needed = total - (len(title) + 2)
+    left = needed // 2
+    right = needed - left
+    return f"{'-'*left} {title} {'-'*right}"
+
+print(args.parser.prog)
 
 size = {
     "x": 10,
     "y": 10
 }
 
-tests:list[TestCase] = [
-    Baseline("Quick-Baseline-10x10", size=size, period=60),
-    GroundTruth("Quick-GroundTruth-10x10", size=size, period=60),
-    StudyResult("Quick-StudyResult-10x10", size=size, period=60),
-    Baseline("Quick-Baseline-3-10x10", size=size, period=60, workload_count=3),
-    GroundTruth("Quick-GroundTruth-3-10x10", size=size, period=60, workload_count=3),
-    StudyResult("Quick-StudyResult-3-10x10", size=size, period=60, workload_count=3),
-    Baseline("Short-Baseline-10x10", size=size, period=60*60*6),
-    GroundTruth("Short-GroundTruth-10x10", size=size, period=60*60*6),
-    StudyResult("Short-StudyResult-10x10", size=size, period=60*60*6),
-    Baseline("Short-Baseline-3-10x10", size=size, period=60*60*6, workload_count=3),
-    GroundTruth("Short-GroundTruth-3-10x10", size=size, period=60*60*6, workload_count=3),
-    StudyResult("Short-StudyResult-3-10x10", size=size, period=60*60*6, workload_count=3),
-    Baseline("Baseline-10x10", size=size),
-    GroundTruth("GroundTruth-10x10", size=size),
-    StudyResult("StudyResult-10x10", size=size),
-    Baseline("Baseline-3-10x10", size=size, workload_count=3),
-    GroundTruth("GroundTruth-3-10x10", size=size, workload_count=3),
-    StudyResult("StudyResult-3-10x10", size=size, workload_count=3)
-]
+tests: list[TestCase] = (
+    make_test_triple("Quick-10x10", size=size, period=60) +
+    make_test_triple("Quick-3-10x10", size=size, period=60, workload_count=3) +
+    make_test_triple("Short-10x10", size=size, period=60*60*6) +
+    make_test_triple("Short-3-10x10", size=size, period=60*60*6, workload_count=3) +
+    make_test_triple("10x10", size=size) +
+    make_test_triple("3-10x10", size=size, workload_count=3)
+)
 
 for test in tests:
     test.cleanup()
     test.kubernetes_setup()
+    print(headline(test.name))
     print(f"test duration: {test.period} seconds")
     test.run()
     # cleanup again to clean up local dependencies
---
+++ tests/Utils.py
@@ -0,0 +1,11 @@
+from lib.TestCase import TestCase
+from tests.Baseline import Baseline
+from tests.GroundTruth import GroundTruth
+from tests.StudyResult import StudyResult
+
+def make_test_triple(name: str, size: dict[str, int] = {"x": 10, "y": 10}, period: int = 86400, delay: int = 25, scale_up: float = .5, scale_down: float = .2, min_replicas: int = 1, max_replicas: int = 10, workload_count: int = 1) -> list[TestCase]:
+    return [
+        Baseline(f"Baseline-{name}", size, period, delay, scale_up, scale_down, min_replicas, max_replicas, workload_count),
+        GroundTruth(f"GroundTruth-{name}", size, period, delay, scale_up, scale_down, min_replicas, max_replicas, workload_count),
+        StudyResult(f"StudyResult-{name}", size, period, delay, scale_up, scale_down, min_replicas, max_replicas, workload_count)
+    ]
