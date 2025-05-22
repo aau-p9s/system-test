f3a721288057c944a26cdfc71345d97b81afab16
Thomas Jensen <mast3r@skade.dev>
2025-05-22 11:25:59 +0200

    integer division

--- lib/Metrics.py
+++ lib/Metrics.py
@@ -9,9 +9,9 @@ def measure_power_usage():
     ])
     cpu_map = {
         name: int(usage[:-1])
-        for name, usage, *_ in [list(filter(lambda e: not e == "", [content for content in line.split(" ")])) for line in usage_raw.split("\n")]
+        for name, usage, *_ in [list(filter(lambda e: not e == "", [content for content in line.split(" ")])) for line in usage_raw.split("\n")[:-1]]
     }
     total_cores = 4
     usage_map = { name: usage / (total_cores * 1000) * 100 for name, usage in cpu_map.items()}
     power_map = { name: usage / 100 * TDP for name, usage in usage_map.items()}
-    return sum(usage_map.values()) / len(usage_map.values()), sum(power_map.values()) / len(power_map.values())
+    return sum(usage_map.values()) // len(usage_map.values()), sum(power_map.values()) // len(power_map.values())
