7ec356aa21baa48fc82f5bb3e9b45a2a4853919f
Thomas Jensen <mast3r@skade.dev>
2025-06-01 02:34:43 +0200

    allow for pure nix execution

--- flake.nix
+++ flake.nix
@@ -1,5 +1,15 @@
 {
     # make flake an exact copy of forecaster
+    inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
     inputs.forecaster.url = "github:aau-p9s/forecaster/fix/wait-for-finish";
-    outputs = { forecaster, ... }: forecaster;
+    inputs.forecaster.inputs.nixpkgs.follows = "nixpkgs";
+    outputs = { forecaster, nixpkgs, ... }: forecaster // (let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+    in {
+        packages.${system}.test = pkgs.writeScriptBin "test" ''
+            #!${pkgs.bash}/bin/bash
+            ${forecaster.packages.${system}.env.interpreter} Test.py $@
+        '';
+    });
 }
