dd3ae24e999b11002705d7dd6176b51b190f217b
Thomas Jensen <mast3r@skade.dev>
2025-06-05 01:03:01 +0200

    reduced wait time if models are already present

--- tests/StudyResult.py
+++ tests/StudyResult.py
@@ -57,7 +57,7 @@ class StudyResult(TestCase):
         print("Discovering services")
         curl(f"localhost:{autoscaler_exposed_port}/services/start", json=False)
         # let shit run
-        logged_delay(120)
+        logged_delay(120 if reinit_db else 5)
 
         services = curl(f"localhost:{autoscaler_exposed_port}/services")
         for name in self.workload_kubeconfigs:
